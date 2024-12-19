8b9fb4953ecc71b31a510a80baaab9f9d2b521f2
sadot <sadotcortes@gmail.com>
2024-12-19 00:40:01 -0600

    chore(keymap): add shorten function to reference keymap set

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,45 +1,47 @@
 -- See `:help vim.keymap.set()`
 
+local keymap = vim.keymap.set
+
 -- files
-vim.keymap.set("n", "QQ", "<cmd>q!<cr>", { noremap = false })
-vim.keymap.set("n", "WW", "<cmd>w!<cr>", { silent = true })
-vim.keymap.set("n", "E", "$", { noremap = false })
-vim.keymap.set("n", "B", "^", { noremap = false })
-vim.keymap.set("n", "ss", "<cmd>noh<CR>", { noremap = true })
+keymap("n", "QQ", "<cmd>q!<cr>", { noremap = false })
+keymap("n", "WW", "<cmd>w!<cr>", { silent = true })
+keymap("n", "E", "$", { noremap = false })
+keymap("n", "B", "^", { noremap = false })
+keymap("n", "ss", "<cmd>noh<CR>", { noremap = true })
 --
 -- plugin manager
-vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { silent = true, desc = "Open Lazy" })
+keymap("n", "<leader>l", "<cmd>Lazy<CR>", { silent = true, desc = "Open Lazy" })
 --
 -- splits when am lazyyy
-vim.keymap.set("n", "vv", "<c-w>v", { silent = true, desc = "Vertical split" })
-vim.keymap.set("n", "vn", "<c-w>n", { silent = true, desc = "Horizontal split" })
+keymap("n", "vv", "<c-w>v", { silent = true, desc = "Vertical split" })
+keymap("n", "vn", "<c-w>n", { silent = true, desc = "Horizontal split" })
 -- --- close
-vim.keymap.set("n", "qq", "<cmd>q<CR>", { silent = true, noremap = true, desc = "Close split" })
+keymap("n", "qq", "<cmd>q<CR>", { silent = true, noremap = true, desc = "Close split" })
 -- only
-vim.keymap.set("n", "<leader>o", "<cmd>:only<CR>", { silent = true, noremap = true, desc = "Only" })
+keymap("n", "<leader>o", "<cmd>:only<CR>", { silent = true, noremap = true, desc = "Only" })
 -- --- move
-vim.keymap.set("n", "<leader>vl", "<c-w>l", { silent = true, noremap = true, desc = "Go right" })
-vim.keymap.set("n", "<leader>vh", "<c-w>j", { silent = true, noremap = true, desc = "Go left" })
-vim.keymap.set("n", "<leader>vj", "<c-w>h", { silent = true, noremap = true, desc = "Go bottom" })
-vim.keymap.set("n", "<leader>vk", "<c-w>k", { silent = true, noremap = true, desc = "Go up" })
+keymap("n", "<leader>vl", "<c-w>l", { silent = true, noremap = true, desc = "Go right" })
+keymap("n", "<leader>vh", "<c-w>j", { silent = true, noremap = true, desc = "Go left" })
+keymap("n", "<leader>vj", "<c-w>h", { silent = true, noremap = true, desc = "Go bottom" })
+keymap("n", "<leader>vk", "<c-w>k", { silent = true, noremap = true, desc = "Go up" })
 -- --- resize
-vim.keymap.set("n", "<C-W>,", "<cmd>vertical resize -10<CR>", { noremap = true })
-vim.keymap.set("n", "<C-W>.", "<cmd>vertical resize +10<CR>", { noremap = true })
+keymap("n", "<C-W>,", "<cmd>vertical resize -10<CR>", { noremap = true })
+keymap("n", "<C-W>.", "<cmd>vertical resize +10<CR>", { noremap = true })
 --
 -- -- nop
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+keymap({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 --
 -- -- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 --
 -- --- Tabs
-vim.keymap.set({ "n" }, "<leader>t", ":tabnew<cr>", { silent = true })
-vim.keymap.set({ "n" }, "<leader>tc", ":tabclose<cr>", { silent = true })
-vim.keymap.set({ "n" }, "<leader>tn", ":tabn<cr>", { silent = true })
-vim.keymap.set({ "n" }, "<leader>tp", ":tabp<cr>", { silent = true })
-vim.keymap.set({ "n" }, "<leader>tf", ":tabfirst<cr>", { silent = true })
-vim.keymap.set({ "n" }, "<leader>tl", ":tablast<cr>", { silent = true })
+keymap({ "n" }, "<leader>t", ":tabnew<cr>", { silent = true })
+keymap({ "n" }, "<leader>tc", ":tabclose<cr>", { silent = true })
+keymap({ "n" }, "<leader>tn", ":tabn<cr>", { silent = true })
+keymap({ "n" }, "<leader>tp", ":tabp<cr>", { silent = true })
+keymap({ "n" }, "<leader>tf", ":tabfirst<cr>", { silent = true })
+keymap({ "n" }, "<leader>tl", ":tablast<cr>", { silent = true })
 --
 -- others
-vim.keymap.set({ "n", "v" }, "<leader>lr", vim.lsp.buf.references, { buffer = true })
+keymap({ "n", "v" }, "<leader>lr", vim.lsp.buf.references, { buffer = true })

318b1f2eaae8fd1ae45343ec5aba1ba009f6c3ed
sadot <sadotcortes@gmail.com>
2024-12-23 19:04:47 -0600

    chore(editor:util): add utils to works with on_attach

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,5 @@
+local utils = require("util.editor")
+
 return {
 	{
 		-- Neovim plugin to manage the file system and other tree like structures.
@@ -109,42 +111,7 @@ return {
 				topdelete = { text = "" },
 				changedelete = { text = "▎" },
 			},
-			on_attach = function(buffer)
-				local gs = package.loaded.gitsigns
-
-				local function map(mode, l, r, desc)
-					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-				end
-
-        -- stylua: ignore start
-        map("n", "]h", function()
-          if vim.wo.diff then
-            vim.cmd.normal({ "]c", bang = true })
-          else
-            gs.nav_hunk("next")
-          end
-        end, "Next Hunk")
-        map("n", "[h", function()
-          if vim.wo.diff then
-            vim.cmd.normal({ "[c", bang = true })
-          else
-            gs.nav_hunk("prev")
-          end
-        end, "Prev Hunk")
-        map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-        map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "[h]unk [s]tage")
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "[h]unk [r]eset")
-        map("n", "<leader>ghS", gs.stage_buffer, "[S]tage Buffer")
-        map("n", "<leader>ghu", gs.undo_stage_hunk, "[U]ndo Stage Hunk")
-        map("n", "<leader>ghR", gs.reset_buffer, "[R]eset Buffer")
-        map("n", "<leader>ghp", gs.preview_hunk_inline, "[p]review Hunk Inline")
-        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "[b]lame Line")
-        map("n", "<leader>ghB", function() gs.blame() end, "[b]lame Buffer")
-        map("n", "<leader>ghd", gs.diffthis, "[d]iff This")
-        map("n", "<leader>ghD", function() gs.diffthis("~") end, "[D]iff This ~")
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-			end,
+			on_attach = utils.on_attach_git_signs,
 		},
 	},
 	{
--- lua/util/editor.lua
+++ lua/util/editor.lua
@@ -6,9 +6,55 @@ local patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package
 
 local M = {}
 function M.get_root_dir()
-  local rootdir = vim.fs.dirname(vim.fs.find(patterns, { upward = true })[1])
+	local rootdir = vim.fs.dirname(vim.fs.find(patterns, { upward = true })[1])
 
-  return rootdir or vim.uv.cwd()
+	return rootdir or vim.uv.cwd()
+end
+
+function M.on_attach_git_signs(buffer)
+	local gs = package.loaded.gitsigns
+
+	local function map(mode, l, r, desc)
+		vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+	end
+
+	map("n", "]h", function()
+		if vim.wo.diff then
+			vim.cmd.normal({ "]c", bang = true })
+		else
+			gs.nav_hunk("next")
+		end
+	end, "Next Hunk")
+	map("n", "[h", function()
+		if vim.wo.diff then
+			vim.cmd.normal({ "[c", bang = true })
+		else
+			gs.nav_hunk("prev")
+		end
+	end, "Prev Hunk")
+	map("n", "]H", function()
+		gs.nav_hunk("last")
+	end, "Last Hunk")
+	map("n", "[H", function()
+		gs.nav_hunk("first")
+	end, "First Hunk")
+	map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "[h]unk [s]tage")
+	map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "[h]unk [r]eset")
+	map("n", "<leader>ghS", gs.stage_buffer, "[S]tage Buffer")
+	map("n", "<leader>ghu", gs.undo_stage_hunk, "[U]ndo Stage Hunk")
+	map("n", "<leader>ghR", gs.reset_buffer, "[R]eset Buffer")
+	map("n", "<leader>ghp", gs.preview_hunk_inline, "[p]review Hunk Inline")
+	map("n", "<leader>ghb", function()
+		gs.blame_line({ full = true })
+	end, "[b]lame Line")
+	map("n", "<leader>ghB", function()
+		gs.blame()
+	end, "[b]lame Buffer")
+	map("n", "<leader>ghd", gs.diffthis, "[d]iff This")
+	map("n", "<leader>ghD", function()
+		gs.diffthis("~")
+	end, "[D]iff This ~")
+	map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 end
 
 return M

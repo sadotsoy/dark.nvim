8f5710b2c94232dc9b4caf00e441ae8df1bb204d
sadot <sadotcortes@gmail.com>
2025-01-07 12:55:35 -0600

    chore(lsp:fidget): ui updated for fidget

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,7 +38,26 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"saghen/blink.cmp",
-			{ "j-hui/fidget.nvim", opts = {} },
+			{
+				"j-hui/fidget.nvim",
+				opts = {
+					notification = {
+						-- Options related to the notification window and buffer
+						window = {
+							normal_hl = "Comment", -- Base highlight group in the notification window
+							winblend = 0, -- Background color opacity in the notification window
+							border = "none", -- Border around the notification window
+							zindex = 45, -- Stacking priority of the notification window
+							max_width = 0, -- Maximum width of the notification window
+							max_height = 0, -- Maximum height of the notification window
+							x_padding = 1, -- Padding from right edge of window boundary
+							y_padding = 0, -- Padding from bottom edge of window boundary
+							align = "bottom", -- How to align the notification window
+							relative = "editor", -- What the notification window position is relative to
+						},
+					},
+				},
+			},
 			"mason.nvim",
 			{ "williamboman/mason-lspconfig.nvim", config = function() end },
 			{

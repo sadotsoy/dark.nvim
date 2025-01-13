95ac182c13bce5134b57f1e1eb22176f991c8a35
sadot <sadotcortes@gmail.com>
2025-01-13 11:12:19 -0600

    fix(autocmd): adjust yank highlight timeout
    Reduced the timeout for yank highlight from 333ms to 123ms to improve
    responsiveness and provide a better visual feedback experience.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,6 +6,6 @@ api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight selection on yank",
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 333 })
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 123 })
 	end,
 })

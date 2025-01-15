e3f05ad1760dd47a814f17cac598bc0e3c10da3a
sadot <sadotcortes@gmail.com>
2025-01-14 18:09:14 -0600

    feat(treesitter): comment out class textobjects mappings
    Commented out the class textobjects mappings in the Treesitter configuration
    to prevent potential conflicts or issues. This change ensures that only the
    function and parameter textobjects mappings are active.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -71,22 +71,22 @@ return {
           enable = true,
           goto_next_start = {
             ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
+            -- ["]c"] = "@class.outer",
             ["]a"] = "@parameter.inner",
           },
           goto_next_end = {
             ["]F"] = "@function.outer",
-            ["]C"] = "@class.outer",
+            -- ["]C"] = "@class.outer",
             ["]A"] = "@parameter.inner"
           },
           goto_previous_start = {
             ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
+            -- ["[c"] = "@class.outer",
             ["[a"] = "@parameter.inner",
           },
           goto_previous_end = {
             ["[F"] = "@function.outer",
-            ["[C"] = "@class.outer",
+            -- ["[C"] = "@class.outer",
             ["[A"] = "@parameter.inner",
           },
         },

37d332ee0ead7eaf59843ce62eb13e24efb92382
sadot <sadotcortes@gmail.com>
2025-01-30 11:24:18 -0600

    feat: update plugin commits and add vgit.nvim config
    - Updated commit hashes for various plugins in lazy-lock.json
    - Corrected comment capitalization for Fugitive.vim in git.lua
    - Added commented-out configuration for vgit.nvim with keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,7 @@ local git_utils = require("util.git")
 
 return {
   {
-    -- fugitive.vim: A Git wrapper so awesome, it should be illegal
+    -- Fugitive.vim: A Git wrapper so awesome, it should be illegal
     "tpope/vim-fugitive",
   },
   {
@@ -46,4 +46,30 @@ return {
       }):map("<leader>uG")
     end,
   },
+  -- {
+  --   -- Visual git plugin for Neovim
+  --   'tanvirtin/vgit.nvim',
+  --   branch = 'v1.0.x',
+  --   -- or               , tag = 'v1.0.2',
+  --   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+  --   -- Lazy loading on 'VimEnter' event is necessary.
+  --   event = 'VimEnter',
+  --   config = function()
+  --     require("vgit").setup({
+  --       keymaps = {
+  --         ['n <C-k>'] = function() require('vgit').hunk_up() end,
+  --         ['n <C-j>'] = function() require('vgit').hunk_down() end,
+  --         ['n <leader>gs'] = function() require('vgit').buffer_hunk_stage() end,
+  --         ['n <leader>gr'] = function() require('vgit').buffer_hunk_reset() end,
+  --         ['n <leader>gp'] = function() require('vgit').buffer_hunk_preview() end,
+  --         ['n <leader>gb'] = function() require('vgit').buffer_blame_preview() end,
+  --         ['n <leader>gf'] = function() require('vgit').buffer_diff_preview() end,
+  --         ['n <leader>gh'] = function() require('vgit').buffer_history_preview() end,
+  --         ['n <leader>gu'] = function() require('vgit').buffer_reset() end,
+  --         ['n <leader>gd'] = function() require('vgit').project_diff_preview() end,
+  --         ['n <leader>gx'] = function() require('vgit').toggle_diff_preference() end,
+  --       },
+  --     })
+  --   end,
+  -- },
 }

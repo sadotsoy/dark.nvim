8b282e4db5d5d10a2a776059da940551e7655a0a
sad_ot <sad@Sas-MacBook-Pro.local>
2025-07-12 10:45:41 -0600

    feat(coding): prioritize lazydev completions in providers

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -115,11 +115,13 @@ return {
         },
         -- add lazydev for yor completion providers
         providers = {
-          -- dont show LuaLS require statements when lazydev has items
-          ---@diagnostic disable-next-line: missing-fields
           lsp = { fallbacks = { "lazydev" } },
-          ---@diagnostic disable-next-line: missing-fields
-          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
           avante_commands = {
             name = "avante_commands",
             module = "blink.compat.source",
